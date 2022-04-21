b04fd92b110d98eab371245a64ed9f53562ff992
hezbims <hezbisulaiman@gmail.com>
2022-04-21 21:41:27 +0800

    Memperdetail main_fragment.xml

--- app/src/main/java/com/example/gratefulnote/mainfragment/MainFragment.kt
+++ app/src/main/java/com/example/gratefulnote/mainfragment/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.example.gratefulnote
+package com.example.gratefulnote.mainfragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gratefulnote.R
 
 
 class MainFragment : Fragment() {
@@ -15,6 +16,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
