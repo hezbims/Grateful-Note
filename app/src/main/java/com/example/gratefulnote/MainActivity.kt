c5b3fdea62902350de828a8760bb0c77336e9da3
hezbims <hezbisulaiman@gmail.com>
2022-06-19 23:02:43 +0800

    Berhasil Mengimplement navigation drawer
    Note : Fungsionalitas item belum diimplementasikan

--- app/src/main/java/com/example/gratefulnote/MainActivity.kt
+++ app/src/main/java/com/example/gratefulnote/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.gratefulnote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController : NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         navController = findNavController(R.id.nav_host)
 
-        NavigationUI.setupActionBarWithNavController(this , navController)
+        // parameter pertama : biar top destination itu yang paling awal di navigation graph
+        // parameter kedua : biar up button nanti punnya fungsionalitas dari drawerlayout (muncul bergeser) kalo dipencet pada di top destination
+        appBarConfiguration = AppBarConfiguration(navController.graph , findViewById<DrawerLayout>(R.id.drawer_layout))
+
+        // biar ada upbutton/drawer button
+        NavigationUI.setupActionBarWithNavController(this , navController , appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp() = navController.navigateUp()
+    override fun onSupportNavigateUp() : Boolean{
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.set_new_password)
+            Toast.makeText(this , "Mencoba ngeset password" , Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
 
 }
