02aa3c2269e1a2c3798bd2589c59ce926c5e35c4
hezbims <hezbisulaiman@gmail.com>
2024-05-26 13:08:01 +0800

    nambahin mock daily alarm setter module, ngegantiin bindvalue karena dianggap repetitif

---
+++ app/src/androidTest/java/com/example/gratefulnote/hilt_module/MockDailyAlarmSetterModule.kt
@@ -0,0 +1,33 @@
+package com.example.gratefulnote.hilt_module
+
+import com.example.gratefulnote.daily_notification._hilt_module.DailyAlarmSetterModule
+import com.example.gratefulnote.daily_notification.domain.service.IDailyAlarmSetter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import org.mockito.Mockito
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DailyAlarmSetterModule::class]
+)
+object MockDailyAlarmSetterModule {
+    @Provides
+    @Singleton
+    fun provideMockDailyReminderAlarmSetter() : IDailyAlarmSetter =
+        Mockito.mock(IDailyAlarmSetter::class.java).apply {
+            Mockito.`when`(
+                this.disableDailyAlarm(Mockito.any(Int::class.java))
+            ).then {}
+            Mockito.`when`(
+                this.enableDailyAlarm(
+                    Mockito.any(Int::class.java),
+                    Mockito.any(Int::class.java),
+                    Mockito.any(Int::class.java)
+                )
+            ).then {}
+        }
+}
--- app/src/androidTest/java/com/example/gratefulnote/test_scenario/daily_reminder/TestDisableItemAlarmCanceled.kt
+++ app/src/androidTest/java/com/example/gratefulnote/test_scenario/daily_reminder/TestDisableItemAlarmCanceled.kt
@@ -2,35 +2,26 @@ package com.example.gratefulnote.test_scenario.daily_reminder
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.example.gratefulnote.MainActivity
-import com.example.gratefulnote.daily_notification._hilt_module.DailyAlarmSetterModule
 import com.example.gratefulnote.daily_notification.domain.service.IDailyAlarmSetter
 import com.example.gratefulnote.database.GratefulNoteDatabase
 import com.example.gratefulnote.robot._common.utils.TestAppDataManager
 import com.example.gratefulnote.robot.daily_reminder.DailyReminderRobot
 import com.example.gratefulnote.robot.main_home.MainHomeRobot
 import com.example.gratefulnote.test_scenario.daily_reminder.test_data.prepareThreeEnabledDailyNotifications
-import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.any
-import org.mockito.Mockito.mock
 import org.mockito.Mockito.never
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
 import javax.inject.Inject
 
 @HiltAndroidTest
-@UninstallModules(DailyAlarmSetterModule::class)
 class TestDisableItemAlarmCanceled {
-
-    @BindValue
-    val mockDailyAlarmSetter: IDailyAlarmSetter = mock(IDailyAlarmSetter::class.java)
-
+    @Inject
+    lateinit var mockDailyAlarmSetter: IDailyAlarmSetter
     @Inject
     lateinit var db : GratefulNoteDatabase
 
@@ -49,14 +40,6 @@ class TestDisableItemAlarmCanceled {
     @Before
     fun prepare(){
         hiltRule.inject()
-        `when`(mockDailyAlarmSetter.disableDailyAlarm(
-            any(Int::class.java))
-        ).then {}
-        `when`(mockDailyAlarmSetter.enableDailyAlarm(
-            any(Int::class.java),
-            any(Int::class.java),
-            any(Int::class.java)
-        )).then {}
 
         appDataManager.clearAppData()
         db.prepareThreeEnabledDailyNotifications()
