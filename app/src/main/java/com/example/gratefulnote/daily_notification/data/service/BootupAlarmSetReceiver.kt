555eabccc84cd063285d4e91484a8ce96025b7f7
hezbims <hezbisulaiman@gmail.com>
2024-07-23 19:48:50 +0800

    ngebetulin bootup alarm set receiver go asyncnya

--- app/src/main/java/com/example/gratefulnote/daily_notification/data/service/BootupAlarmSetReceiver.kt
+++ app/src/main/java/com/example/gratefulnote/daily_notification/data/service/BootupAlarmSetReceiver.kt
@@ -6,26 +6,15 @@ import android.content.Intent
 import com.example.gratefulnote.common.domain.receiverGoAsync
 import com.example.gratefulnote.daily_notification.domain.service.IDailyNotificationManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.coroutines.EmptyCoroutineContext
 
 @AndroidEntryPoint
 class BootupAlarmSetReceiver : BroadcastReceiver() {
     @Inject
     lateinit var dailyNotificationManager : IDailyNotificationManager
-
-
     override fun onReceive(context: Context, intent: Intent) = receiverGoAsync {
         if (intent.action != Intent.ACTION_BOOT_COMPLETED)
             return@receiverGoAsync
-        goAsync()
-        @OptIn(DelicateCoroutinesApi::class)
-        GlobalScope.launch(EmptyCoroutineContext) {
-            dailyNotificationManager.setAlarmForAllEnabledDailyNotifications()
-        }
-
+        dailyNotificationManager.setAlarmForAllEnabledDailyNotifications()
     }
 }
